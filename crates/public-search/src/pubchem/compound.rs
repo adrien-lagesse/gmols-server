ea1940a93fd113c95c1b0410c98acf3ad51cb35f
GitButler <gitbutler@gitbutler.com>
2024-04-12 03:10:48 +0200

    GitButler Integration Commit
    This is an integration commit for the virtual branches that GitButler is tracking.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - dev (refs/gitbutler/dev)
       branch head: bf9100d4e50cf5fb493ae1f16ab92109e052ecb5
       - .dockerignore
       - compose.yaml
       - Dockerfile
       - crates/public-search/src/pubchem/compound.rs
       - crates/public-search/src/pubchem/mod.rs
       - src/main.rs
       - crates/public-search/src/lib.rs
       - Cargo.lock
       - .rustfmt.toml
       - crates/public-search/Cargo.toml
       - Cargo.toml
       - crates/public-search/README.md
    
    Your previous branch was: refs/heads/main
    
    The sha for that commit was: 9c3fe64d541242437224b4e7efa310062a4c63de
    
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

---
+++ crates/public-search/src/pubchem/compound.rs
@@ -0,0 +1,26 @@
+use serde;
+
+#[derive(Debug, serde::Serialize)]
+pub struct Compound {
+    cid: u32,
+    name: String,
+    iupac_name: String,
+    description: Option<String>,
+    molecular_formula: String,
+    canonical_smiles: String,
+    molecular_weight: f32,
+}
+
+impl Compound {
+    pub fn new(
+        cid: u32,
+        name: String,
+        iupac_name: String,
+        description: Option<String>,
+        molecular_formula: String,
+        canonical_smiles: String,
+        molecular_weight: f32,
+    ) -> Compound {
+        Compound { cid, name, iupac_name, description, molecular_formula, canonical_smiles, molecular_weight }
+    }
+}
---
+++ crates/public-search/src/pubchem/mod.rs
@@ -0,0 +1,76 @@
+mod compound;
+use anyhow;
+use anyhow::Context;
+pub use compound::Compound;
+use reqwest;
+use serde_json::Value as JSON;
+
+const PUBCHEM_PROLOG: &str = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/";
+
+pub struct Client {
+    reqwest_client: reqwest::Client,
+}
+
+impl Client {
+    pub fn new() -> Client {
+        let client = reqwest::Client::builder().build().unwrap();
+        Client { reqwest_client: client }
+    }
+
+    pub async fn get_compound_by_cid(&self, cid: u32) -> anyhow::Result<compound::Compound> {
+        let response = self
+            .reqwest_client
+            .get(
+                format!("{PUBCHEM_PROLOG}/compound/cid/{cid}/property/MolecularFormula,MolecularWeight,CanonicalSMILES,IUPACName,Title/JSON")
+            )
+            .send()
+            .await?;
+
+        let result: JSON = response.json().await?;
+
+        let name = result
+            .pointer("/PropertyTable/Properties/0/Title")
+            .and_then(|s| s.as_str())
+            .context("Can't find title")?;
+        let iupac_name = result
+            .pointer("/PropertyTable/Properties/0/IUPACName")
+            .and_then(|s| s.as_str())
+            .context("Can't find IUPACName")?;
+        let molecular_formula = result
+            .pointer("/PropertyTable/Properties/0/MolecularFormula")
+            .and_then(|s| s.as_str())
+            .context("Can't find MolecularFormula")?;
+        let canonical_smiles = result
+            .pointer("/PropertyTable/Properties/0/CanonicalSMILES")
+            .and_then(|s| s.as_str())
+            .context("Can't find CanonicalSMILES")?;
+        let molecular_weight: f32 = result
+            .pointer("/PropertyTable/Properties/0/MolecularWeight")
+            .and_then(|s| s.as_str())
+            .and_then(|s| s.parse().ok())
+            .context("Can't find MolecularWeight")?;
+
+        let response = self
+            .reqwest_client
+            .get(format!("{PUBCHEM_PROLOG}/compound/cid/{cid}/description/JSON"))
+            .send()
+            .await?;
+
+        let result: JSON = response.json().await?;
+
+        let description = result
+            .pointer("/InformationList/Information/1/Description")
+            .and_then(|v| v.as_str())
+            .map(|s| s.to_string());
+
+        Ok(Compound::new(
+            cid,
+            name.to_string(),
+            iupac_name.to_string(),
+            description,
+            molecular_formula.to_string(),
+            canonical_smiles.to_string(),
+            molecular_weight,
+        ))
+    }
+}
